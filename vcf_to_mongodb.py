59b3d35c6ed91b38e0275fde7c34aefe3ce2ecf0
Carlijn Fransen <carlijn.fransen@radboudumc.nl>
2020-06-09 15:01:30 +0200

    requirements added as well as database thing

--- vcf_to_mongodb.py
+++ vcf_to_mongodb.py
@@ -6,25 +6,33 @@ import vcf
 
 
 def main():
-    read_vcf()
-    posts = create_db_connection()
+    con = create_db_connection()
+    read_vcf(con)
 
-
-def read_vcf():
-    vcf_reader = vcf.Reader(open('D:\\BIN-1920\\gnomad.exomes.r2.1.1.sites.13.vcf'))
+def read_vcf(con):
+    vcf_reader = vcf.Reader(open('D:/BIN-1920/gnomad.exomes.r2.1.1.sites.13.vcf'))
     for record in vcf_reader:
-        for x in record.INFO:
-            # print(x)
-            if x == 'non_cancer_AF':
-
-        break
+        chrom = record.CHROM
+        pos = record.POS
+        ref = record.REF
+        alt = record.ALT
+        try:
+            non_cancer_af = record.INFO['non_cancer_AF'][0]
+        except KeyError:
+            pass
+            #print (record.INFO)
+        cancer_af = calculate_cancer_AF(non_cancer_af)
+        benign = filter_for_benign(cancer_af)
+        if benign:
+            document = create_document(chrom, pos, ref, alt, cancer_af)
+            add_document(document, con)
 
 
 def create_db_connection():
-    client = pymongo.MongoClient("localhost", 27017)
-    db = client.cancer_api
-    posts = db.posts
-    return (posts)
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client['cancer_api']
+    col =  db['exome_data']
+    return (col)
 
 
 def calculate_cancer_AF(non_cancer_af):
@@ -50,14 +58,14 @@ def create_document(chrom, pos, ref, alt, cancer_af):
     doc_dict["ref"] = ref
     doc_dict["alt"] = alt
     doc_dict["cancer_af"] = cancer_af
+    return doc_dict
 
-def add_document():
+def add_document(document, con):
     # adds document as json/dictionary style format to the MongoDB database
-    pass
+    con.insert_one(document)
 
 
-def create_db():
-    pass
+
 
 
 main()
