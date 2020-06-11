7187ed6eacdb8e4cc16d66cddcb17ea0ce296695
Ilse den Brok, Bin-3d <31927559+IlsedenBrok@users.noreply.github.com>
2020-06-11 20:38:38 +0200

    deleted output file generator and added commentary

--- API_logic.py
+++ API_logic.py
@@ -1,10 +1,21 @@
-
-import json
+"""
+Author: Ilse den Brok
+Date: 11 June 2020
+Function: makes a structured list from a file given by the user
+"""
 import csv
 
 
+def main():
+    input_attribute = read_input_file()
+    input_list = structure_list(input_attribute)
+
+
 def read_input_file():
-    # input file is file send to api
+    """
+    the input file is split into its header and attributes
+    :return: input_attribute is a list with information from the user
+    """
     input_file = "input_file.txt"
     with open(input_file, 'r') as file:
         reader = csv.reader(file)
@@ -21,7 +32,12 @@ def read_input_file():
 
 
 def structure_list(input_attribute):
-    # a structured list filled with the input data is made
+    """
+    makes a structured list with attributes in the form
+    'chromosome_position_reference nucleotide_alternative nucleotide'
+    :param input_attribute:
+    :return: input_list is the structured list with all complete instances
+    """
     input_list = []
     for line in input_attribute:
         if len(line) == 4:
@@ -29,16 +45,8 @@ def structure_list(input_attribute):
             input_list.append(regel)
             print(regel)
         else:
-            # change this to api popup
             print('One or more of your entries did not contain all 4 elements, these will be ignored')
     return input_list
 
 
-def return_json():
-    #data = {}
-    # chrom, pos, freq, ref, alt, benign, cancer = opbouw
-    with open('output.json', 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
-
-
-
+main()
