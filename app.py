f068e48744204b029a7921f4da939a3bec4cfd4a
Ilse den Brok, Bin-3d <31927559+IlsedenBrok@users.noreply.github.com>
2020-06-11 15:56:43 +0200

    updated app.py to include templates

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/")
+def index():
+    return render_template('home.html')
 
 
-if __name__ == '__main__':
+@app.route("/results")
+def index():
+    return render_template('results.html')
+
+
+# @app.route('/request', methods=['GET'])
+# def test():
+#     chromosome = request.args.get('chrom')
+#     print(chromosome)
+#     position = request.args.get('pos')
+#     print(position)
+#     nucleotide = request.args.get('nt')
+#     print(nucleotide)
+#     return render_template('results.html', chrom_pos_1=chromosome, pos_pos_1=position, nucleo_pos_1=nucleotide)
+#
+
+if __name__ == "__main__":
     app.run()
