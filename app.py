444b7fb4e5598b5447dbf5369c9fbf7b8bb055c8
Ilse den Brok, Bin-3d <31927559+IlsedenBrok@users.noreply.github.com>
2020-06-11 17:04:17 +0200

    added browse button

--- app.py
+++ app.py
@@ -1,28 +1,26 @@
 from flask import Flask, render_template, request
+from werkzeug import secure_filename
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
+@app.route("/", endpoint='func1')
+def func1():
     return render_template('home.html')
 
 
-@app.route("/results")
-def index():
+@app.route("/results", methods=['POST'], endpoint='func2')
+def func2():
     return render_template('results.html')
 
 
-# @app.route('/request', methods=['GET'])
-# def test():
-#     chromosome = request.args.get('chrom')
-#     print(chromosome)
-#     position = request.args.get('pos')
-#     print(position)
-#     nucleotide = request.args.get('nt')
-#     print(nucleotide)
-#     return render_template('results.html', chrom_pos_1=chromosome, pos_pos_1=position, nucleo_pos_1=nucleotide)
-#
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+    return 'file uploaded successfully'
+
 
 if __name__ == "__main__":
     app.run()
